9e26c5784ccaf1385942b013b297a0d9e0ae1f33
Jeffrey E Blake Jr <jeffrey.e.blake@gmail.com>
2020-01-19 20:29:49 -0600

    Refactorting
    Refactored 3 functions to testUtility.py

--- projects/blakejef/dominion/testDominion2.py
+++ projects/blakejef/dominion/testDominion2.py
@@ -8,9 +8,11 @@ Created on Sat Jan 18 2020
 import Dominion
 import random
 from collections import defaultdict
+import testUtility
 
 #Get player names
-player_names = ["Annie","*Ben","*Carla"]
+#player_names = ["Annie","*Ben","*Carla"]
+player_names = testUtility.GetPlayers()
 
 #number of curses and victory cards
 if len(player_names)>2:
@@ -19,39 +21,9 @@ else:
     nV=8
 nC = -10 + 10 * len(player_names)
 
-#Define box
-box = {}
-box["Woodcutter"]=[Dominion.Woodcutter()]*10
-box["Smithy"]=[Dominion.Smithy()]*10
-box["Laboratory"]=[Dominion.Laboratory()]*10
-box["Village"]=[Dominion.Village()]*10
-box["Festival"]=[Dominion.Festival()]*10
-box["Market"]=[Dominion.Market()]*10
-box["Chancellor"]=[Dominion.Chancellor()]*10
-box["Workshop"]=[Dominion.Workshop()]*10
-box["Moneylender"]=[Dominion.Moneylender()]*10
-box["Chapel"]=[Dominion.Chapel()]*10
-box["Cellar"]=[Dominion.Cellar()]*10
-box["Remodel"]=[Dominion.Remodel()]*10
-box["Adventurer"]=[Dominion.Adventurer()]*10
-box["Feast"]=[Dominion.Feast()]*10
-box["Mine"]=[Dominion.Mine()]*10
-box["Library"]=[Dominion.Library()]*10
-box["Gardens"]=[Dominion.Gardens()]*nV
-box["Moat"]=[Dominion.Moat()]*10
-box["Council Room"]=[Dominion.Council_Room()]*10
-box["Witch"]=[Dominion.Witch()]*10
-box["Bureaucrat"]=[Dominion.Bureaucrat()]*10
-box["Militia"]=[Dominion.Militia()]*10
-box["Spy"]=[Dominion.Spy()]*10
-box["Thief"]=[Dominion.Thief()]*10
-box["Throne Room"]=[Dominion.Throne_Room()]*10
+box = testUtility.GetBoxes(nV)
 
-supply_order = {0:['Curse','Copper'],2:['Estate','Cellar','Chapel','Moat'],
-                3:['Silver','Chancellor','Village','Woodcutter','Workshop'],
-                4:['Gardens','Bureaucrat','Feast','Militia','Moneylender','Remodel','Smithy','Spy','Thief','Throne Room'],
-                5:['Duchy','Market','Council Room','Festival','Laboratory','Library','Mine','Witch'],
-                6:['Gold','Adventurer'],8:['Province']}
+supply_order = testUtility.SupplyOrder()
 
 #Pick 10 cards from box to be in the supply.
 boxlist = [k for k in box]
